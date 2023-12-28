c8152709468d74962f265e78585570e358f8442d
LucinaBueno <lucina.bueno@jwmsolutions.com>
2023-12-28 14:17:02 -0600

    #7 Feature: Set rules for price and inventory on initial product load and sync events
    - Add a waiting time before getting information from ct (strong consistency).

--- event/src/ordergroove/client/ct-products-api.ts
+++ event/src/ordergroove/client/ct-products-api.ts
@@ -4,7 +4,15 @@ import { createApiRoot } from '../../client/create.client';
 import { GetFunction } from '../../types/index.types';
 
 export const productProjectionsSearch: GetFunction<ProductProjectionPagedQueryResponse> = async (queryArgs) => {
+  await sleep(10000); // Wait for the changes to propagate in ct
+
   const { body } = await createApiRoot().productProjections().search().get({ queryArgs }).execute();
 
   return body;
+};
+
+const sleep = async (milliseconds: number) => {
+  await new Promise(resolve => {
+    return setTimeout(resolve, milliseconds)
+  });
 };
