d5ffeeac6e46ea4b2801b179fb03f0a9a49f3205
LucinaBueno <lucina.bueno@jwmsolutions.com>
2024-01-04 10:05:31 -0600

    - Prefix for testing.

--- job/src/ordergroove/helpers/product-helper.ts
+++ job/src/ordergroove/helpers/product-helper.ts
@@ -28,7 +28,9 @@ export const convertProductProjectionToOrdergrooveProducts = async (productProje
         break;
       }
 
-      const masterVariantSku = result.masterVariant.sku === undefined ? '' : result.masterVariant.sku;
+      const prPrefix = process.env.PR_PREFIX === undefined ? '' : process.env.PR_PREFIX;
+
+      const masterVariantSku = result.masterVariant.sku === undefined ? '' : prPrefix + result.masterVariant.sku;
       const masterVariantPrice = getScopedPrice(result.masterVariant.scopedPrice);
 
       if (masterVariantPrice === undefined) {
@@ -48,7 +50,7 @@ export const convertProductProjectionToOrdergrooveProducts = async (productProje
 
       const variants: Array<ProductVariant> =  Object.values(result.variants);
       for (let variant of result.variants) {
-        const variantSku = variant.sku === undefined ? '' : variant.sku;
+        const variantSku = variant.sku === undefined ? '' : prPrefix + variant.sku;
 
         const variantPrice = getScopedPrice(variant.scopedPrice);
 
