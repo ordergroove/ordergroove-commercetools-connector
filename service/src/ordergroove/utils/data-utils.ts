271fec6e84b72d13222f18da4ef45324c3d97353
LucinaBueno <lucina.bueno@jwmsolutions.com>
2024-01-13 11:40:02 -0600

    #14 Certification fix: Avoid using var and UUID generation
    - Change the implemetation to generate the UUID.
    - Remove flag from test script.

--- service/src/ordergroove/utils/data-utils.ts
+++ service/src/ordergroove/utils/data-utils.ts
@@ -1,3 +1,5 @@
+import crypto from 'crypto';
+
 export const addDecimalPointToCentAmount = (centAmount: number, fractionDigits: number): number => {
   const amount = String(centAmount);
   const intAmount = amount.slice(0, (fractionDigits * -1));
@@ -7,11 +9,5 @@ export const addDecimalPointToCentAmount = (centAmount: number, fractionDigits: 
 }
 
 export const createUUID = () : string => {
-  var dt = new Date().getTime();
-  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = (dt + Math.random()*16)%16 | 0;
-    dt = Math.floor(dt/16);
-    return (c=='x' ? r :(r&0x3|0x8)).toString(16);
-  });
-  return uuid;
+  return crypto.randomUUID();
 }
