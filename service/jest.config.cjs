0bf26383ac2f6b7e50ed7959ffb2e7c989a585d8
LucinaBueno <lucina.bueno@jwmsolutions.com>
2024-01-17 14:41:24 -0600

    #17 Certification fix: In the job app change the URL parameter for a custom object in commercetools
    - Remove service app

--- service/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  displayName: 'Tests Typescript Application - Job',
-  moduleDirectories: ['node_modules', 'src'],
-  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
-  testPathIgnorePatterns: ['/build/'],
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
